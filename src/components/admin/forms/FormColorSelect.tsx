e340ab15764da098d2720a9d95a628c9365cf558
Ruutuli <rudhuli@gmail.com>
2025-12-17 09:06:29 -0500

    Import csvOptions data into FormColorSelect component for enhanced dropdown functionality

--- src/components/admin/forms/FormColorSelect.tsx
+++ src/components/admin/forms/FormColorSelect.tsx
@@ -3,6 +3,7 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import { UseFormRegisterReturn, UseFormSetValue, useFormContext } from 'react-hook-form';
 import { useDropdownOptions } from '@/hooks/useDropdownOptions';
+import { csvOptions } from '@/lib/utils/csvOptionsData';
 
 interface FormColorSelectProps {
   register: UseFormRegisterReturn;
